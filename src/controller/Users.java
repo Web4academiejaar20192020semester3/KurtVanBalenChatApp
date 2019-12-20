d18148a8da98b6a6362def7bed1999f29930f561
Kurt Van Balen (HOGESCHOOL UCLL) <vbalen85@imec.be>
2019-12-20 11:31:19 +0100

    Objectmapper changed to stringbuffer

--- src/controller/Users.java
+++ src/controller/Users.java
@@ -6,10 +6,13 @@ import domain.Person;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Users extends RequestHandler {
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        Controller.setIsJson(true);
         if (request.getParameter("id") != null) {
             Person user = getPersonService().getPerson(request.getParameter("id"));
             ArrayList<Person> array = new ArrayList<>();
@@ -20,16 +23,24 @@ public class Users extends RequestHandler {
     }
 
     private String toJSON(ArrayList<Person> array) {
-        ObjectMapper mapper = new ObjectMapper();
-        String json = "";
-        try
-        {
-            json = mapper.writeValueAsString(array);
+        StringBuffer stringBuffer = new StringBuffer();
+        List<Person> users = array;
+        Iterator iterator = users.iterator();
+        stringBuffer.append("{ \"Person\":[");
+        while (iterator.hasNext()) {
+            Person user = (Person) iterator.next();
+
+            stringBuffer.append("{");
+            stringBuffer.append("\"name\": \"" + user.getName() + "\",");
+            stringBuffer.append("\"status\": \"" + user.getStatus() + "\"," );
+            stringBuffer.append("\"id\": \"" + user.getUserId() + "\"" );
+            stringBuffer.append("}");
+            if (iterator.hasNext()){
+                stringBuffer.append(",");
+            }
         }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        return json;
+        stringBuffer.append("]");
+        stringBuffer.append("}");
+        return stringBuffer.toString();
     }
 }
